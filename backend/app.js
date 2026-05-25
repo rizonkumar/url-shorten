6c6e88b83384275d10f6d7104930a9463b3e7284
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2026-05-25 09:19:00 +0530

    Add monorepo with frontend and backend

--- backend/app.js
+++ backend/app.js
@@ -20,10 +20,10 @@ app.use(express.json());
 app.use("/api", shortUrlRoute);
 app.get("/:shortUrl([a-zA-Z0-9_-]{7})", getOriginalUrl);
 
-app.use(express.static(path.join(__dirname, "frontend/dist")));
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
 app.use((err, req, res, next) => {
